435b144bdf7581b4dfb5a323d5b88d48826cc866
biberairport <dv@fastmail.de>
2022-12-07 18:03:40 +0100

    day04

--- day04/day04.py
+++ day04/day04.py
@@ -3,10 +3,10 @@ r1 = r2 = 0
 
 with open("day04/day04.txt", 'r') as fh:
     for x in fh:
-        x = re.split('[,-]', x)
+        x = (list(map(int,re.split('[,-]', x))))
 #task1
-        if(int(x[0]) <= int(x[2]) and int(x[1]) >= int(x[3]) or int(x[0]) >= int(x[2]) and int(x[1]) <= int(x[3])): r1 += 1
+        if(x[0] <= x[2] and x[1] >= x[3] or x[0] >= x[2] and x[1] <= x[3]): r1 += 1
 #taks2
-        if(int(x[0]) <= int(x[2]) and int(x[1]) >= int(x[2]) or int(x[0]) >= int(x[2]) and int(x[1]) <= int(x[3]) or int(x[0]) <= int(x[3]) and int(x[1]) >= int(x[3])): r2 += 1   
+        if(x[0] <= x[2] and x[1] >= x[2] or x[0] >= x[2] and x[1] <= x[3] or x[0] <= x[3] and x[1] >= x[3]): r2 += 1   
 
 print(r1, r2)
